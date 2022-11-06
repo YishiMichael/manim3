fix: fix wrong calculation of view model

src/cameras/camera.py
@@ -11,11 +11,12 @@ __all__ = ["Camera"]
 
 class Camera(ABC):
     def __init__(self: Self):
-        self.matrix: pyrr.Matrix44 = pyrr.Matrix44.look_at(np.array([0.0, 0.0, 5.0]), np.array([0.0, 0.0, 0.0]), np.array([0.0, 1.0, 0.0]))
+        self.view_matrix: pyrr.Matrix44 = pyrr.Matrix44.look_at(np.array([0.0, 5.0, 0.0]), np.array([0.0, 0.0, 0.0]), np.array([0.0, 0.0, 1.0]))  # TODO
+        #self.view_matrix: pyrr.Matrix44 = pyrr.Matrix44.look_at(np.array([0.0, 0.0, 5.0]), np.array([0.0, 0.0, 0.0]), np.array([0.0, 1.0, 0.0]))  # TODO
 
     @abstractmethod
     def get_projection_matrix(self: Self) -> pyrr.Matrix44:
         raise NotImplementedError
 
-    def get_transform_matrix(self: Self) -> pyrr.Matrix44:
-        return self.get_projection_matrix() @ ~self.matrix
+    def get_view_matrix(self: Self) -> pyrr.Matrix44:
+        return self.view_matrix

src/geometries/parametrized_surface_geometry.py
@@ -20,36 +20,28 @@ class ParametrizedSurfaceGeometry(Geometry):
         self.u_samples: FloatArrayType = u_samples
         self.v_samples: FloatArrayType = v_samples
 
-    def get_indices(self: Self) -> VertexIndicesType:
+    def get_vertex_indices(self: Self) -> VertexIndicesType:
         u_len = len(self.u_samples)
         v_len = len(self.v_samples)
-        uv_grid = np.mgrid[0:u_len, 0:v_len]
-        a = uv_grid[:, :-1, :-1]
-        b = uv_grid[:, +1:, :-1]
-        c = uv_grid[:, :-1, +1:]
-        d = uv_grid[:, +1:, +1:]
+        index_grid = np.mgrid[0:u_len, 0:v_len]
+        ne = index_grid[:, +1:, +1:]
+        nw = index_grid[:, :-1, +1:]
+        sw = index_grid[:, :-1, :-1]
+        se = index_grid[:, +1:, :-1]
         return np.ravel_multi_index(
-            np.stack((b, a, d, a, c, d), axis=-1),
+            tuple(np.stack((se, sw, ne, sw, nw, ne), axis=-1)),
             (u_len, v_len)
-        ).flatten()#.astype(np.uint32)  # TODO: convert type
+        ).flatten().astype(np.int32)
 
-    def get_vertex_attributes(self: Self) -> AttributesType:
-        func = self.func
-        vertices = np.array([
-            func(u, v)
-            for v in self.v_samples
-            for u in self.u_samples
-        ])
-        #x, y = np.meshgrid(
-        #    np.linspace(-1.0, 1.0, self.x_segments + 1),
-        #    np.linspace(-1.0, 1.0, self.y_segments + 1)
-        #)
-        #vertices = np.stack(
-        #    (x, y, np.zeros_like(x)),
-        #    axis=-1
-        #).reshape((-1, 3))
-        result = np.zeros(len(vertices), dtype=[
-            ("in_position", np.float32, (3,))  # TODO: convert type
-        ])
-        result["in_position"] = vertices
-        return result
+    def get_attributes_v(self: Self) -> AttributesItemType:
+        uv_grid = np.meshgrid(self.u_samples, self.v_samples, indexing="ij")
+        return np.fromiter((
+            (
+                self.func(u, v),
+                np.array([u, v])
+                # derivatives for normal vectors...
+            ) for u, v in np.nditer(uv_grid)
+        ), dtype=np.dtype([
+            ("in_position", np.float32, (3,)),
+            ("in_uv", np.float32, (2,))
+        ]))

src/materials/basic_material.py
@@ -1,21 +1,43 @@
+import numpy as np
+
 from cameras.camera import Camera
 from materials.material import Material
 from utils.typing import *
 
 
 class BasicMaterial(Material):
-    def __init__(self: Self, color: ColorArrayType):
+    def __init__(
+        self: Self,
+        color: ColorArrayType | None = None,
+        map: TextureArrayType | None = None
+    ):
         super().__init__()
+        if color is None:
+            color = np.ones(4)
         self.color: ColorArrayType = color
+        self.map: TextureArrayType | None = map
 
     def get_define_macros(self: Self) -> list[str]:
-        return []
+        result = []
+        if self.map is not None:
+            result.append("USE_UV")
+            result.append("USE_MAP")
+        return result
 
-    def get_uniforms(self: Self, camera: Camera) -> dict[str, UniformType]:
+    def get_attributes_r(self: Self, camera: Camera) -> AttributesItemType:
+        return np.array([(
+            camera.get_projection_matrix(),
+            camera.get_view_matrix(),
+            self.color,
+            0
+        )], dtype=np.dtype([
+            ("in_projection_matrix", (np.float32, (4, 4))),
+            ("in_view_matrix", (np.float32, (4, 4))),
+            ("in_color", (np.float32, (4,))),
+            ("in_map", (np.int8, (1,)))
+        ]))
+
+    def get_texture_dict(self: Self) -> dict[int, TextureArrayType | None]:
         return {
-            "uniform_transform_matrix": camera.get_transform_matrix(),
-            "uniform_color": self.color,
+            0: self.map
         }
-
-    def get_texture_arrays(self: Self) -> list[TextureArrayType]:
-        return []

src/utils/typing.py
@@ -1,28 +1,36 @@
+from enum import Enum
 from typing import Any, Literal, Union
 
 import numpy as np
 
 
 __all__ = [
-	"Vector2Type",
-	"Vector3Type",
-	"Vector4Type",
-	"Matrix33Type",
-	"Matrix44Type",
-	"IntArrayType",
-	"FloatArrayType",
-	"Vector2ArrayType",
-	"Vector3ArrayType",
-	"Vector4ArrayType",
-	"Matrix33ArrayType",
-	"Matrix44ArrayType",
-	"ColorArrayType",
-	"UniformType",
-	"TextureArrayType",
-	"AttributesType",
-	"VertexIndicesType",
-	"Self"
+    "AttributeUsage",
+    "Vector2Type",
+    "Vector3Type",
+    "Vector4Type",
+    "Matrix33Type",
+    "Matrix44Type",
+    "IntArrayType",
+    "FloatArrayType",
+    "Vector2ArrayType",
+    "Vector3ArrayType",
+    "Vector4ArrayType",
+    "Matrix33ArrayType",
+    "Matrix44ArrayType",
+    "ColorArrayType",
+    "TextureArrayType",
+    "AttributesItemType",
+    "AttributesDictType",
+    "VertexIndicesType",
+    "Self"
 ]
+
+
+class AttributeUsage(Enum):
+    V = 0  # per vertex
+    I = 1  # per instance
+    R = 2  # per render
 
 
 Vector2Type = np.ndarray[tuple[Literal[2]], np.dtype[np.float64]]
@@ -40,9 +48,11 @@ Matrix33ArrayType = np.ndarray[tuple[int, Literal[3], Literal[3]], np.dtype[np.f
 Matrix44ArrayType = np.ndarray[tuple[int, Literal[4], Literal[4]], np.dtype[np.float64]]
 
 ColorArrayType = Vector4Type
-UniformType = Union[int, float, np.ndarray[tuple[int, ...], np.dtype[np.float64]]]
+#UniformType = Union[int, float, np.ndarray[tuple[int, ...], np.dtype[np.float64]]]
 TextureArrayType = np.ndarray[tuple[int, int, Literal[4]], np.dtype[np.uint8]]
-AttributesType = np.ndarray[tuple[int], np.dtype[Any]]
-VertexIndicesType = np.ndarray[tuple[int], np.dtype[np.uint32]]
+#AttributesType = np.ndarray[tuple[int], np.dtype[Any]]
+AttributesItemType = np.ndarray[tuple[int], np.dtype[Any]]
+AttributesDictType = dict[AttributeUsage, AttributesItemType]
+VertexIndicesType = IntArrayType
 
 Self = Any  # This shall be removed when advanced to py 3.11