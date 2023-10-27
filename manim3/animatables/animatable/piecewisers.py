64ac0c20bdf498c67641adf8dcdf65cf38f30854
YishiMichael <MichaelW@sjtu.edu.cn>
2023-10-27 00:01:31 -0400

    fix: fix EvenPiecewiser.piecewise

--- manim3/animatables/animatable/piecewisers.py
+++ manim3/animatables/animatable/piecewisers.py
@@ -36,58 +36,67 @@ class EvenPiecewiser(Piecewiser):
     ) -> PiecewiseData:
         n_segments = self._n_segments
         backwards = self._backwards
-        segment_center = self.get_segment_center(alpha) * (-1.0 if backwards else 1.0)
-        segment_length = float(np.clip(self.get_segment_length(alpha), 0.0, 1.0))
-        segment_start = segment_center - segment_length / 2.0
-        segment_stop = segment_center + segment_length / 2.0
+        #center = self.get_center(alpha) * (-1.0 if backwards else 1.0)
+        #length = float(np.clip(self.get_length(alpha), 0.0, 1.0))
+        #start = center - length / 2.0
+        #stop = center + length / 2.0
+        start, stop = self.get_segment(alpha)
+        assert 0.0 <= stop - start <= 1.0
+        if backwards:
+            start, stop = -stop, -start
 
-        boundaries = np.array((segment_start, segment_stop))
-        wrap_flag = int(segment_stop) - int(segment_start)
-        #if backwards:
-        #    wrap_flag = 1 - wrap_flag
-        wrap_shift = 2 * int(segment_start % n_segments) + wrap_flag
-        split_alphas = np.linspace(boundaries, boundaries + 1.0, n_segments, endpoint=False).flatten()
-        split_alphas = np.roll(split_alphas, wrap_shift)
-        split_alphas[:wrap_shift] -= np.floor(segment_stop)
-        split_alphas[wrap_shift:] -= np.floor(segment_start)
-        concatenate_indices = np.arange(1 - wrap_flag, 2 * n_segments + 1, 2)
+        start_offset = start % 1.0
+        stop_offset = start_offset + (stop - start)
+        if stop_offset > 1.0:
+            start_offset, stop_offset = stop_offset - 1.0, start_offset
+            first_index = 0
+        else:
+            first_index = 1
+
+        split_alphas = np.column_stack(tuple(
+            np.arange(n_segments, dtype=np.float64) + offset
+            for offset in (start_offset, stop_offset)
+        )).flatten() / n_segments
+        concatenate_indices = np.arange(first_index, 2 * n_segments + 1, 2)
+        #start_floor = np.floor(start)
+        #stop_floor = np.floor(stop)
+        #split_starts = np.linspace(0.0, 1.0, n_segments, endpoint=False) + start_offset
+        #split_stops = np.linspace(0.0, 1.0, n_segments, endpoint=False) + stop_offset
+        #split_start_cutoff = int(start % n_segments)
+        #split_stops = np.linspace(stop, stop + 1.0, n_segments, endpoint=False)
+        #boundaries = np.array((start, stop))
+        #wrap_flag = int(stop) - int(start)
+        ##if backwards:
+        ##    wrap_flag = 1 - wrap_flag
+        #wrap_shift = 2 * int(start % n_segments) + wrap_flag
+        #split_alphas = np.linspace(boundaries, boundaries + 1.0, n_segments, endpoint=False).flatten()
+        #split_alphas = np.roll(split_alphas, wrap_shift)
+        #split_alphas[:wrap_shift] -= np.floor(stop)
+        #split_alphas[wrap_shift:] -= np.floor(start)
+        #concatenate_indices = np.arange(1 - wrap_flag, 2 * n_segments + 1, 2)
         #if backwards:
         #    split_alphas = 1.0 - split_alphas[::-1]
-        print(segment_center, segment_length, split_alphas, concatenate_indices)
         return PiecewiseData(
             split_alphas=split_alphas,
             concatenate_indices=concatenate_indices
         )
 
     @abstractmethod
-    def get_segment_center(
+    def get_segment(
         self: Self,
         alpha: float
-    ) -> float:
-        pass
-
-    @abstractmethod
-    def get_segment_length(
-        self: Self,
-        alpha: float
-    ) -> float:
+    ) -> tuple[float, float]:
         pass
 
 
 class PartialPiecewiser(EvenPiecewiser):
     __slots__ = ()
 
-    def get_segment_center(
+    def get_segment(
         self: Self,
         alpha: float
-    ) -> float:
-        return alpha / 2.0
-
-    def get_segment_length(
-        self: Self,
-        alpha: float
-    ) -> float:
-        return alpha
+    ) -> tuple[float, float]:
+        return (0.0, alpha)
 
 
 class FlashPiecewiser(EvenPiecewiser):
@@ -106,19 +115,12 @@ class FlashPiecewiser(EvenPiecewiser):
         )
         self._proportion: float = proportion
 
-    def get_segment_center(
+    def get_segment(
         self: Self,
         alpha: float
-    ) -> float:
+    ) -> tuple[float, float]:
         proportion = self._proportion
-        return (min(alpha * (1.0 + proportion), 1.0) + max(alpha * (1.0 + proportion) - proportion, 0.0)) / 2.0
-
-    def get_segment_length(
-        self: Self,
-        alpha: float
-    ) -> float:
-        proportion = self._proportion
-        return min(alpha * (1.0 + proportion), 1.0) - max(alpha * (1.0 + proportion) - proportion, 0.0)
+        return (min(alpha * (1.0 + proportion), 1.0), max(alpha * (1.0 + proportion) - proportion, 0.0))
 
 
 class DashedPiecewiser(EvenPiecewiser):
@@ -137,17 +139,12 @@ class DashedPiecewiser(EvenPiecewiser):
         )
         self._proportion: float = proportion
 
-    def get_segment_center(
+    def get_segment(
         self: Self,
         alpha: float
-    ) -> float:
-        return alpha
-
-    def get_segment_length(
-        self: Self,
-        alpha: float
-    ) -> float:
-        return self._proportion
+    ) -> tuple[float, float]:
+        proportion = self._proportion
+        return (alpha, alpha + proportion)
 
 
 class Piecewisers:
